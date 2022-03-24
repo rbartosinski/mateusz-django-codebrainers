adfa0c35d7a3c5b768d04cd09d7a8c2c46fc1a4f
rbartosinski <r.bartosinski@wp.pl>
2022-03-24 16:14:16 +0100

    2nd commit/required changes

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.auth.models import User, Group
+from django import forms
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -10,7 +11,7 @@ from django.utils.datastructures import MultiValueDictKeyError
 from django.views import generic, View
 
 from .forms import AppointmentForm, AppointmentEntryForm
-from .models import Service, Appointment
+from .models import Service, Appointment, Employee
 
 
 class IndexView(LoginRequiredMixin, View):
@@ -18,13 +19,15 @@ class IndexView(LoginRequiredMixin, View):
     def get(self, request):
         return render(request, 'polls/index.html')
 
+
 class ServicesView(View):
     def get(self, request):
         servicesList = Service.objects.all()
-        context ={
+        context = {
             'servicesList': servicesList
         }
         return render(request, 'polls/services.html', context)
+
 
 class DetailView(View):
     def get(self, request, pk):
@@ -34,13 +37,15 @@ class DetailView(View):
         }
         return render(request, 'polls/detail.html', context)
 
+
 class StaffView(View):
     def get(self, request):
         employees = User.objects.all()
-        context={
+        context = {
             'employees': employees
         }
         return render(request, 'polls/staff.html', context)
+
 
 class StaffDetailView(View):
     def get(self, request, pk):
@@ -56,40 +61,39 @@ class Book2View(View):
     def get(self, request):
         form = AppointmentEntryForm()
         context = {
-            'form' : form,
+            'form': form,
         }
-        return render(request,'polls/book2.html', context )
-    def post(self,request,context):
+        return render(request, 'polls/book2.html', context)
+
+    def post(self, request):
         form = AppointmentEntryForm(request.POST)
         if form.is_valid():
             appointment_service = form.cleaned_data['appointment_service']
-        # context = {
-        #     'appointment_service' : appointment_service
-        # }
-        # return render(request, 'polls/book.html', context)
-        request.session['appointment_name'] = appointment_name
+            request.session['appointment_service'] = str(appointment_service.id)
         return HttpResponseRedirect(reverse('polls:book'))
 
+
 class BookView(View):
-    def get(self,request):
-        #appointment = Service.objects.get(appointment_name=request.session['appointment_name'])
-        #appointment.
+    def get(self, request):
         form = AppointmentForm()
+        form.fields['appointment_employee'] = forms.ModelChoiceField(
+            Employee.objects.filter(employee_service=request.session['appointment_service']))
         context = {
-            'form' : form,
+            'form': form,
 
         }
-        return render(request,'polls/book.html', context )
-    def post(self, request):
-        form = AppointmentForm(request.POST)
-        appointment = False
-        if form.is_valid():
-            appointment = Appointment.objects.create(
-            appointment_name=form.cleaned_data['appointment_name'],
-            appointment_surname = form.cleaned_data['appointment_surname'],
-            appointment_email = form.cleaned_data['appointment_email'],
-            appointment_date = form.cleaned_data['appointment_date'],
-            appointment_service = 'appointment_service',
-            )
+        return render(request, 'polls/book.html', context)
 
+    def post(self, request):
+        service = Service.objects.get(id=request.session['appointment_service'])
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            Appointment.objects.create(
+                appointment_name=form.cleaned_data['appointment_name'],
+                appointment_surname=form.cleaned_data['appointment_surname'],
+                appointment_email=form.cleaned_data['appointment_email'],
+                appointment_date=form.cleaned_data['appointment_date'],
+                appointment_service=service,
+            )
+        messages.success(request, 'Great, your on {} visit is now reserved'.format(form.cleaned_data['appointment_date']))
         return HttpResponseRedirect(reverse('polls:index'))
